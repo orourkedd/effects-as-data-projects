042149c0b5b858fd531931e72329496218a77b89
psfrankie <frankie-orourke@pluralsight.com>
2017-04-14 08:26:06 -0700

    expire zen cache after 5 seconds

--- express/src/functions/zen.js
+++ express/src/functions/zen.js
@@ -1,26 +1,34 @@
 const actions = require('../actions')
 const { success, isFailure } = require('effects-as-data')
 
+//  Get wisdom from github and cache for 5 seconds before you have to learn new wisdom
+
 function * zen () {
-  //  Check the in-memory cache for some zen
-  //  @see https://github.com/orourkedd/effects-as-data#getstate
-  const cached = yield actions.getState(['zen'])
+  //  Check the in-memory cache for some zen.  Run these actions in parallel
+  //  For actions.getState, @see https://github.com/orourkedd/effects-as-data#getstate
+  //  For actions.now, @see https://github.com/orourkedd/effects-as-data#now
+  const fiveSeconds = 1000 * 5
+  const [now, state] = yield [actions.now(), actions.getState(['zen', 'time'])]
   //  @see https://github.com/orourkedd/simple-protocol for understanding `payload`
-  if (cached.payload.zen) return { body: cached.payload.zen }
+  const cacheIsFresh = now.payload - state.payload.time < fiveSeconds
+  if (state.payload.zen && cacheIsFresh) {
+    return { body: state.payload.zen }
+  }
 
   //  Get some zen
-  //  @see https://github.com/orourkedd/effects-as-data#httpget
+  //  @see For actions.httpGet, https://github.com/orourkedd/effects-as-data#httpget
   const zen = yield actions.httpGet('https://api.github.com/zen')
-  //  @see https://github.com/orourkedd/simple-protocol for understanding `isFailure`
+  //  @see For isFailure, https://github.com/orourkedd/simple-protocol
   if (isFailure(zen)) return zen
 
   //  Write to in-memory cache
   //  If this fails, we still want to return "zen".  effects-as-data will
   //  automatically handle this failure so it doesn't need to be handled here.
-  //  @see https://github.com/orourkedd/effects-as-data#logging-action-failures for handling failures universally
-  //  @see https://github.com/orourkedd/effects-as-data#setstate for the setState action
+  //  @see For handling failures automatically, https://github.com/orourkedd/effects-as-data#logging-action-failures
+  //  @see For actions.setState, https://github.com/orourkedd/effects-as-data#setstate for the setState action
   yield actions.setState({
-    zen: zen.payload
+    zen: zen.payload,
+    time: now.payload
   })
 
   return {
--- express/src/functions/zen.spec.js
+++ express/src/functions/zen.spec.js
@@ -5,26 +5,45 @@ const { testIt } = require('effects-as-data/test')
 
 const testZen = testIt(zen)
 
-test('zen() should return zen from the cache', testZen(() => {
+test('zen() should return zen from the cache if the cache is less than 5 seconds old', testZen(() => {
+  const cacheTime = 1492180435000
+  const now = cacheTime + 2 * 1000
+
   return [
-    [null, actions.getState(['zen'])],
-    [{ zen: 'foo' }, { body: 'foo' }]
+    [null, [actions.now(), actions.getState(['zen', 'time'])]],
+    [[now, { zen: 'foo', time: cacheTime }], { body: 'foo' }]
   ]
 }))
 
-test('zen() should cache miss, get zen from github, save to cache, and return', testZen(() => {
+test('zen() should cache miss if cache is empty', testZen(() => {
+  const now = 1492180435000
+
   return [
-    [null, actions.getState(['zen'])],
-    [{ zen: null }, actions.httpGet('https://api.github.com/zen')],
-    ['foo', actions.setState({ zen: 'foo' })],
+    [null, [actions.now(), actions.getState(['zen', 'time'])]],
+    [[now, { zen: null, time: null }], actions.httpGet('https://api.github.com/zen')],
+    ['foo', actions.setState({ zen: 'foo', time: now })],
+    [null, { body: 'foo' }]
+  ]
+}))
+
+test('zen() should cache miss if cache is older than 5 seconds, get zen from github, save to cache, and return', testZen(() => {
+  const cacheTime = 1492180435000
+  const now = cacheTime + 10 * 1000
+
+  return [
+    [null, [actions.now(), actions.getState(['zen', 'time'])]],
+    [[now, { zen: 'foo', time: cacheTime }], actions.httpGet('https://api.github.com/zen')],
+    ['foo', actions.setState({ zen: 'foo', time: now })],
     [null, { body: 'foo' }]
   ]
 }))
 
 test('zen() should return http GET failure', testZen(() => {
+  const now = 1492180435000
+
   return [
-    [null, actions.getState(['zen'])],
-    [{ zen: null }, actions.httpGet('https://api.github.com/zen')],
+    [null, [actions.now(), actions.getState(['zen', 'time'])]],
+    [[now, { zen: null, time: null }], actions.httpGet('https://api.github.com/zen')],
     [failure('oops'), failure('oops')]
   ]
 }))
