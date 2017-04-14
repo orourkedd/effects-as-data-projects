e3b4014ac97abf7a7212b3b8ac3b9b56f62983a0
psfrankie <frankie-orourke@pluralsight.com>
2017-04-13 20:15:33 -0700

    add comments to actions and handlers

--- express/src/actions/index.js
+++ express/src/actions/index.js
@@ -1,3 +1,14 @@
 const { actions } = require('effects-as-data/node')
 
+//  effects-as-data has several built-in actions/handlers making
+//  it easy to perform and test complex, side effect producing operations:
+//  @see https://github.com/orourkedd/effects-as-data#actions-packaged-with-effects-as-data
+
+//  You can add your own actions here by
+//  merging them into the actions object.  Actions
+//  will require a corresponding handler (@see /handlers).
+//  Action creating functions return JSON objects that
+//  describe side effects; handlers accept these actions
+//  and perfom the described side effect.
+
 module.exports = actions
--- express/src/handlers/index.js
+++ express/src/handlers/index.js
@@ -1,3 +1,13 @@
 const { handlers } = require('effects-as-data/node')
 
+//  effects-as-data has several built-in actions/handlers making
+//  it easy to perform and test complex, side effect producing operations:
+//  @see https://github.com/orourkedd/effects-as-data#actions-packaged-with-effects-as-data
+
+//  You can add your own handlers here by
+//  merging them into the handlers object.  Handlers
+//  receive actions (@see /actions) and perform side
+//  effects described by those actions.
+
+
 module.exports = handlers
